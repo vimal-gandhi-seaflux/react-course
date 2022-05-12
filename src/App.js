93c7aeae5776f2f7d34c1719e49b35779b49e671
Vimal Gandhi <vimal.gandhi@seaflux.tech>
2022-05-12 11:56:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,48 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
+
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-      <ExpenseItem Date={'2022,12,1'}  Amount={200} Title={'toilerpaper'}/>
-      <ExpenseItem Date={'2022,12,1'} Amount={20} Title={"toilerpaper"}/>
-      <ExpenseItem Date={'2022,12,1'} Amount={20} Title={"toilerpaper"}/> 
+      <ExpenseItem 
+      title = {expenses[0].title}
+      amount = {expenses[0].amount}
+      date = {expenses[0].date}
+      />
+      <ExpenseItem 
+      title = {expenses[1].title}
+      amount = {expenses[1].amount}
+      date = {expenses[1].date}
+      />
+      <ExpenseItem 
+      title = {expenses[2].title}
+      amount = {expenses[2].amount}
+      date = {expenses[2].date}
+      />
       </header>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -25,10 +25,10 @@ function ExpenseItem(props) {
 
   return (
     <div className='expense-item'>
-      <div>{props.Date}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{props.Title}</h2>
-        <div className='expense-item__price'>${props.Amount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
